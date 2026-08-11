86ab56e343e854e234ab5f264ee1f53732766815
asdf57 <53836417+asdf57@users.noreply.github.com>
2026-08-10 21:50:58 -0400

    Prototype etcd watch and controller startup wiring

--- cmd/homelab-controller/main.go
+++ cmd/homelab-controller/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
@@ -13,6 +14,7 @@ import (
 	"github.com/asdf57/prov-controller-test/go/internal/api"
 	"github.com/asdf57/prov-controller-test/go/internal/config"
 	etcdstore "github.com/asdf57/prov-controller-test/go/internal/store/etcd"
+	"go.etcd.io/etcd/api/v3/mvccpb"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
@@ -39,9 +41,46 @@ func run() error {
 	if err != nil {
 		return err
 	}
-	defer etcdClient.Close()
+	defer func(etcdClient *clientv3.Client) {
+		err := etcdClient.Close()
+		if err != nil {
+			slog.Error("etcd client close", "error", err)
+		}
+	}(etcdClient)
+
+	go func() {
+		watchChan := etcdClient.Watch(context.Background(), configuration.EtcdPrefix, clientv3.WithPrefix(), clientv3.WithPrevKV())
+		for watchResp := range watchChan {
+			if err := watchResp.Err(); err != nil {
+				log.Printf("Watch error: %v", err)
+				continue
+			}
+
+			for _, event := range watchResp.Events {
+				switch event.Type {
+				case mvccpb.PUT:
+					slog.Info("PUT event", "key", string(event.Kv.Key), "value", event.Kv.Value)
+					if event.PrevKv != nil {
+						slog.Info("Prev val", "value", event.PrevKv.Value)
+					}
+				case mvccpb.DELETE:
+					slog.Info("DELETE event", "key", string(event.Kv.Key))
+					if event.PrevKv != nil {
+						slog.Info("Prev val", "value", event.PrevKv.Value)
+					}
+
+				}
+			}
+		}
+	}()
 
 	resourceStore := etcdstore.New(etcdClient, configuration.EtcdPrefix)
+
+	// Controller subsystem wiring is intentionally disabled until Store.Watch
+	// and Manager.Serve are implemented.
+	//
+	// controllerStore := controller.NewControllerStore(resourceStore)
+
 	handler := api.New(logger, resourceStore, configuration.RequestTimeout)
 	server := &http.Server{
 		Addr:              configuration.HTTPAddr,
@@ -50,15 +89,38 @@ func run() error {
 		IdleTimeout:       60 * time.Second,
 	}
 
+	// Top level context
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
+	// machineReportController := machine.NewMachineReportController()
+	// manager := controller.NewManager(
+	// 	controllerStore,
+	// 	[]controller.Registration{
+	// 		{
+	// 			Name:           "machine-report-controller",
+	// 			ReconcilesKind: "MachineReport",
+	// 			Watches:        []controller.Watch{{Kind: "MachineReport"}},
+	// 			ResyncPeriod:   5 * time.Minute,
+	// 			Reconciler:     machineReportController,
+	// 		},
+	// 	},
+	// )
+	//
+	// controllerError := make(chan error, 1)
+	// go func() {
+	// 	logger.Info("starting controller subsystem")
+	// 	controllerError <- manager.Serve(ctx)
+	// }()
+
+	// Create a channel for the HTTP server (so we can exit if/when it fails)
 	serverError := make(chan error, 1)
 	go func() {
 		logger.Info("HTTP server starting", "address", configuration.HTTPAddr, "etcdEndpoints", configuration.EtcdEndpoints)
 		serverError <- server.ListenAndServe()
 	}()
 
+	// This is where the main go routine will hang for the majority of the API's lifetime
 	select {
 	case err := <-serverError:
 		if !errors.Is(err, http.ErrServerClosed) {
@@ -67,6 +129,9 @@ func run() error {
 		return nil
 	case <-ctx.Done():
 		logger.Info("shutdown requested")
+		// Enable this case with the controller subsystem wiring above.
+		// case err := <-controllerError:
+		// 	return err
 	}
 
 	shutdownContext, cancel := context.WithTimeout(context.Background(), configuration.ShutdownTimeout)
